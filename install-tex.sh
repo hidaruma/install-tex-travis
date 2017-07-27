2a89b8260ba0120db21a654319f0a4c3c2083f5c
hidaruma <hidaruma@outlook.jp>
2017-07-27 13:50:52 +0900

    Update 2017

--- install-tex.sh
+++ install-tex.sh
@@ -7,7 +7,7 @@ mkdir $DIRNAME
 cd $DIRNAME
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-  curl -L -O http://mirrors.acm.jhu.edu/ctan/systems/mac/mactex/mactex-basictex-20161009.pkg
+  curl -L -O http://ctan.math.washington.edu/tex-archive/systems/mac/mactex/BasicTeX.pkg
   sudo installer -pkg BasicTeX.pkg -target /
   rm BasicTeX.pkg
   export PATH=$PATH:/Library/TeX/texbin
@@ -18,19 +18,19 @@ else
   cd install-tl-*
   cat << EOF > ./small.profile
 selected_scheme scheme-small
-TEXDIR $HOME/texlive/2016
+TEXDIR $HOME/texlive/2017
 TEXMFCONFIG $HOME/.texlive2016/texmf-config
 TEXMFHOME $HOME/texmf
 TEXMFLOCAL $HOME/texlive/texmf-local
-TEXMFSYSCONFIG $HOME/texlive/2016/texmf-config
-TEXMFSYSVAR $HOME/texlive/2016/texmf-var
+TEXMFSYSCONFIG $HOME/texlive/2017/texmf-config
+TEXMFSYSVAR $HOME/texlive/2017/texmf-var
 TEXMFVAR $HOME/.texlive2016/texmf-var
 binary_x86_64-linux 1
 option_doc 0
 option_src 0
 EOF
   ./install-tl -profile ./small.profile -repository http://ctan.mirror.rafal.ca/systems/texlive/tlnet/
-  export PATH=$PATH:$HOME/texlive/2016/bin/x86_64-linux
+  export PATH=$PATH:$HOME/texlive/2017/bin/x86_64-linux
   tlmgr init-usertree
   cd ..
 fi
